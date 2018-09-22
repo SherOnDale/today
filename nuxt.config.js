a898cb9bb50fcb25fcc3c8f2ea3eb9094760ea32
Sherin Binu <sherinbinu@hotmail.com>
2018-09-22 13:47:52 +0530

    creates a simple landing page

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'My Blog',
+    title: 'What I Learned Today',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -25,7 +25,7 @@ module.exports = {
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#FFFFFF' },
+  loading: { color: '#FF0000' },
 
   /*
   ** Global CSS
