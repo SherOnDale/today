f62a7d0ca9396d603d7f1bc62399edb3e84200be
Sherin Binu <sherinbinu@hotmail.com>
2019-03-24 21:40:22 +0530

    set up blog

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -11,36 +11,52 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: 'What I Learned Today',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'My Blog of everything new I learn each day'
+      }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Open+Sans'
+      }
+    ]
   },
 
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: '#fa923f', height: '4px', throttle: 0 },
 
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['~assets/styles/main.css'],
 
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: ['~plugins/index.js', '~plugins/date-filter.js'],
 
   /*
    ** Nuxt.js modules
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
+    [
+      '@nuxtjs/axios',
+      {
+        baseURL: 'https://nuxt-blog-13c98.firebaseio.com/',
+        browserBaseURL: 'https://nuxt-blog-13c98.firebaseio.com/'
+      }
+    ],
     '@nuxtjs/pwa'
   ],
   /*
@@ -58,5 +74,14 @@ export default {
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
+  },
+
+  env: {
+    fbAPIKey: 'AIzaSyDeQ8zBDXxN4Ut9Xvze6IUi3RdRuaMEOds'
+  },
+
+  transition: {
+    name: 'fade',
+    mode: 'out-in'
   }
 };
